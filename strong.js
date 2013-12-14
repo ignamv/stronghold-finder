db3bc9902fb6938feeb133fe0958885034ffc086
Ignacio Martinez Vazquez <ignamv@gmail.com>
2013-12-14 10:08:06 -0300

    Make sure solution is inside bounding rectangle

--- strong.js
+++ strong.js
@@ -42,52 +42,28 @@ function refreshMap() {
     if(measurements.length < 2) {
         return;
     }
-    // Find bounding rectangle
-    var minx = Infinity, maxx = -Infinity, miny = Infinity, maxy = -Infinity;
-    for(var ii=0; ii<measurements.length; ii++) {
-        var option = measurements[ii];
-        minx = Math.min(minx,Math.min(option.x1,option.x2))
-        miny = Math.min(miny,Math.min(option.y1,option.y2))
-        maxx = Math.max(maxx,Math.max(option.x1,option.x2))
-        maxy = Math.max(maxy,Math.max(option.y1,option.y2))
-    }
-    console.log('['+minx+','+maxx+']x['+miny+','+maxy+']');
-    ctx.save();
-    var scale = 1./Math.max((maxx-minx)/canvas.width,
-                            (maxy-miny)/canvas.height);
-    ctx.scale(scale, scale);
-    ctx.lineWidth = 1./scale;
-    ctx.font = '' + 20./scale + 'px serif';
-    ctx.translate(-minx, -miny);
     // Linear least squares problem comes down to a matrix equation
     // (A B)(x) = (E)
     // (B D)(y)   (F)
     var A=0,B=0,C=0,D=0,E=0,F=0;
+    var minx = Infinity, maxx = -Infinity, miny = Infinity, maxy = -Infinity;
     for(var ii=0; ii<measurements.length; ii++) {
-        var curr = measurements[ii];
-        var dirx = curr.x2-curr.x1;
-        var diry = curr.y2-curr.y1;
+        var current = measurements[ii];
+        // Find bounding rectangle
+        minx = Math.min(minx,Math.min(current.x1,current.x2))
+        miny = Math.min(miny,Math.min(current.y1,current.y2))
+        maxx = Math.max(maxx,Math.max(current.x1,current.x2))
+        maxy = Math.max(maxy,Math.max(current.y1,current.y2))
+        var dirx = current.x2-current.x1;
+        var diry = current.y2-current.y1;
         var norm = Math.sqrt(dirx*dirx+diry*diry);
-        dirx /= norm;
-        diry /= norm;
-        ctx.beginPath()
-        ctx.moveTo(curr.x1,curr.y1);
-        ctx.lineTo(curr.x2,curr.y2);
-        ctx.stroke();
-        var arrowLength = 10/scale, arrowWidth = 5/scale;
-        ctx.beginPath();
-        ctx.moveTo(curr.x2,curr.y2);
-        ctx.lineTo(curr.x2-arrowLength*dirx-arrowWidth*diry,
-                   curr.y2-arrowLength*diry+arrowWidth*dirx);
-        ctx.lineTo(curr.x2-arrowLength*dirx+arrowWidth*diry,
-                   curr.y2-arrowLength*diry-arrowWidth*dirx);
-        ctx.lineTo(curr.x2,curr.y2);
-        ctx.fill();
+        current.dirx = dirx/norm;
+        current.diry = diry/norm;
         // Build up normal equation matrix
         A += diry*diry;
         B += -dirx*diry;
         D += dirx*dirx;
-        var cross = curr.x1*diry-curr.y1*dirx;
+        var cross = current.x1*diry-current.y1*dirx;
         E += diry*cross;
         F += -dirx*cross;
     }
@@ -96,10 +72,44 @@ function refreshMap() {
     if(determinant < 1e-9) {
         // TODO: add proper condition check
         console.log('No solution');
+        solx = NaN;
+        soly = NaN;
     } else {
         var solx = (E*D-B*F)/determinant;
+        minx = Math.min(minx, solx);
+        maxx = Math.max(maxx, solx);
         var soly = (A*F-B*E)/determinant;
+        miny = Math.min(miny, soly);
+        maxy = Math.max(maxy, soly);
         console.log('Solution at ('+solx+','+soly+')');
+    }
+    console.log('['+minx+','+maxx+']x['+miny+','+maxy+']');
+    ctx.save();
+    var scale = 1./Math.max((maxx-minx)/canvas.width,
+                            (maxy-miny)/canvas.height);
+    ctx.scale(scale, scale);
+    ctx.lineWidth = 1./scale;
+    ctx.font = '' + 20./scale + 'px serif';
+    ctx.translate(0.5*(canvas.width/scale-minx-maxx),
+                  0.5*(canvas.height/scale-miny-maxy));
+    console.log(ctx.mozCurrentTransform);
+    for(var ii=0; ii<measurements.length; ii++) {
+        var current = measurements[ii];
+        ctx.beginPath()
+        ctx.moveTo(current.x1,current.y1);
+        ctx.lineTo(current.x2,current.y2);
+        ctx.stroke();
+        var arrowLength = 10/scale, arrowWidth = 5/scale;
+        ctx.beginPath();
+        ctx.moveTo(current.x2,current.y2);
+        ctx.lineTo(current.x2-arrowLength*current.dirx-arrowWidth*current.diry,
+                   current.y2-arrowLength*current.diry+arrowWidth*current.dirx);
+        ctx.lineTo(current.x2-arrowLength*current.dirx+arrowWidth*current.diry,
+                   current.y2-arrowLength*current.diry-arrowWidth*current.dirx);
+        ctx.lineTo(current.x2,current.y2);
+        ctx.fill();
+    }
+    if(!isNaN(solx)) {
         var crossSize = 5 / scale;
         ctx.beginPath();
         ctx.moveTo(solx-crossSize,soly);
